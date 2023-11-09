f5bea7d34970b35a5153de1cc7548f3f8db28ed3
Fertilesoil <gasnbloop@gmail.com>
2023-11-09 13:16:16 -0300

    Implementação do consumo de Categorias CRUD

--- projeto_final_bloco_03/src/App.tsx
+++ projeto_final_bloco_03/src/App.tsx
@@ -1,6 +1,11 @@
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import Home from './components/home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './pages/navbar/Navbar';
+import Footer from './pages/footer/Footer';
+import Home from './pages/home/Home'
+import ListaCategorias from './components/categoria/ListaCategorias/ListaCategorias';
+import FormCategoria from './components/categoria/FormCategoria/FormCategoria';
+import DeletaCategoria from './components/categoria/DeletaCategoria/DeletaCategoria';
+
 
 import './index.css'
 
@@ -8,11 +13,19 @@ import './index.css'
 function App() {
 
   return (
-    <>
+
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/categorias" element={<ListaCategorias />} />
+        <Route path="/cadastroCategoria" element={<FormCategoria />} />
+        <Route path="/editarCategoria/:id" element={<FormCategoria />} />
+        <Route path="/deletarCategoria/:id" element={<DeletaCategoria />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
+
   )
 }
 
